13231302598c48f4ccc2c4a834ac483459d17a21
DrChungo <litingo1000@gmail.com>
2025-10-30 20:35:00 +0100

    Update showTasks.php

--- showTasks.php
+++ showTasks.php
@@ -1,10 +1,6 @@
 <?php
 function showTasks($data)
 {
-    if (!isset($data['tasks']) || !is_array($data['tasks'])) {
-        echo "No hay tareas.\n";
-        return;
-    }
 
     foreach ($data['tasks'] as $task) {
         $status = !empty($task["completed"]) ? "Completada" : "Pendiente";
